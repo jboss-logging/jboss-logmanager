ce45dcbedef07333ecce04fa82ec20085e4f4d32
Jason T. Greene <jason.greene@redhat.com>
2012-07-10 22:54:55 -0500

    Color Support

--- src/main/java/org/jboss/logmanager/formatters/PatternFormatter.java
+++ src/main/java/org/jboss/logmanager/formatters/PatternFormatter.java
@@ -29,10 +29,12 @@ public class PatternFormatter extends MultistepFormatter {
 
     private volatile String pattern;
 
+    private volatile ColorMap colors;
     /**
      * Construct a new instance.
      */
     public PatternFormatter() {
+        this.colors = ColorMap.DEFAULT_COLOR_MAP;
     }
 
     /**
@@ -41,8 +43,22 @@ public class PatternFormatter extends MultistepFormatter {
      * @param pattern the initial pattern
      */
     public PatternFormatter(String pattern) {
-        super(FormatStringParser.getSteps(pattern));
+        super(FormatStringParser.getSteps(pattern, ColorMap.DEFAULT_COLOR_MAP));
+        this.colors = ColorMap.DEFAULT_COLOR_MAP;
         this.pattern = pattern;
+    }
+
+    /**
+     * Construct a new instance.
+     *
+     * @param pattern the initial pattern
+     * @param colors the color map to use
+     */
+    public PatternFormatter(String pattern, String colors) {
+        ColorMap colorMap = ColorMap.create(colors);
+        this.colors = colorMap;
+        this.pattern = pattern;
+        setSteps(FormatStringParser.getSteps(pattern, colorMap));
     }
 
     /**
@@ -60,7 +76,65 @@ public class PatternFormatter extends MultistepFormatter {
      * @param pattern the pattern
      */
     public void setPattern(final String pattern) {
-        setSteps(FormatStringParser.getSteps(pattern));
+        setSteps(FormatStringParser.getSteps(pattern, colors));
         this.pattern = pattern;
+    }
+
+    /**
+     * Set the color map to use for log levels when %K{level} is used.
+     *
+     * <p>The format is level:color,level:color,...
+     *
+     * <p>Where level is either a numerical value or one of the following constants:</p>
+     *
+     * <table>
+     *     <tr><td>fatal</td></tr>
+     *     <tr><td>error</td></tr>
+     *     <tr><td>severe</td></tr>
+     *     <tr><td>warn</td></tr>
+     *     <tr><td>warning</td></tr>
+     *     <tr><td>info</td></tr>
+     *     <tr><td>config</td></tr>
+     *     <tr><td>debug</td></tr>
+     *     <tr><td>trace</td></tr>
+     *     <tr><td>fine</td></tr>
+     *     <tr><td>finer</td></tr>
+     *     <tr><td>finest</td></tr>
+     * </table>
+     *
+     * <p>Color is one of the following constants:</p>
+     *
+     * <table>
+     *     <tr><td>clear</td></tr>
+     *     <tr><td>black</td></tr>
+     *     <tr><td>red</td></tr>
+     *     <tr><td>green</td></tr>
+     *     <tr><td>yellow</td></tr>
+     *     <tr><td>blue</td></tr>
+     *     <tr><td>magenta</td></tr>
+     *     <tr><td>cyan</td></tr>
+     *     <tr><td>white</td></tr>
+     *     <tr><td>brightblack</td></tr>
+     *     <tr><td>brightred</td></tr>
+     *     <tr><td>brightgreen</td></tr>
+     *     <tr><td>brightyellow</td></tr>
+     *     <tr><td>brightblue</td></tr>
+     *     <tr><td>brightmagenta</td></tr>
+     *     <tr><td>brightcyan</td></tr>
+     *     <tr><td>brightwhite</td></tr>
+     * </table>
+     *
+     * @param colors a colormap expression string described above
+     */
+    public void setColors(String colors) {
+        ColorMap colorMap = ColorMap.create(colors);
+        this.colors = colorMap;
+        if (pattern != null) {
+            setSteps(FormatStringParser.getSteps(pattern, colorMap));
+        }
+    }
+
+    public String getColors() {
+        return this.colors.toString();
     }
 }
