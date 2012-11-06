c69b737442e10084d2fab39041ec262d6f0c5374
Marcus Klimstra <mgb.klimstra@gmail.com>
2012-11-06 15:26:57 +0100

    This makes more sense.

--- src/test/java/org/jboss/logmanager/formatters/FormattersTests.java
+++ src/test/java/org/jboss/logmanager/formatters/FormattersTests.java
@@ -21,24 +21,38 @@ public class FormattersTests {
 	}
 
 	public void getJarName_module() throws MalformedURLException {
-		String classResourceName = "org/alib/foo/Bar.class";
+		String classResourceName = "org/blib/foo/Bar.class";
 		URL resource = new URL(null, "module:blib-3.14.jar", new DummyURLStreamHandler());
 		
 		assertEquals(Formatters.getJarName(resource, classResourceName), "blib-3.14.jar");
 	}
 
 	public void getJarName_vfs() throws MalformedURLException {
-		String classResourceName = "org/alib/foo/Bar.class";
+		String classResourceName = "org/clib/foo/Bar.class";
 		URL resource = new URL(null, "vfs:/D:/Java/jboss-as-7.1.1.Final/standalone/deployments/myapp.war/WEB-INF/lib/clib-3.1.4.jar", new DummyURLStreamHandler());
 		
 		assertEquals(Formatters.getJarName(resource, classResourceName), "clib-3.1.4.jar");
 	}
 
-	public void getJarName_other() throws MalformedURLException {
-		String classResourceName = "org/alib/foo/Bar.class";
+	public void getJarName_other1() throws MalformedURLException {
+		String classResourceName = "org/dlib/foo/Bar.class";
 		URL resource = new URL(null, "foo://bar/" + classResourceName, new DummyURLStreamHandler());
 		
-		assertEquals(Formatters.getJarName(resource, classResourceName), "Bar.class");
+		assertEquals(Formatters.getJarName(resource, classResourceName), "");
+	}
+	
+	public void getJarName_other2() throws MalformedURLException {
+		String classResourceName = "org/elib/foo/Bar.class";
+		URL resource = new URL(null, "foo://bar/quux/" + classResourceName, new DummyURLStreamHandler());
+		
+		assertEquals(Formatters.getJarName(resource, classResourceName), "quux");
+	}
+	
+	public void getJarName_other3() throws MalformedURLException {
+		String classResourceName = "org/flib/foo/Bar.class";
+		URL resource = new URL(null, "foo://bar/baz/quux/" + classResourceName, new DummyURLStreamHandler());
+		
+		assertEquals(Formatters.getJarName(resource, classResourceName), "quux");
 	}
 	
 }
