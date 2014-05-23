600f15cc9a8fda57d1a12471f0c4294edd14d5c2
James Perkins <jperkins@redhat.com>
2014-05-23 14:49:01 -0700

    [LOGMGR-106] Add hack to set level cache for OpenJDK and Java 7+.

---
+++ src/test/java/org/jboss/logmanager/LogManagerTests.java
@@ -0,0 +1,77 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.logmanager;
+
+import java.lang.reflect.Method;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author <a href="mailto:jperkins@redhat.com">James R. Perkins</a>
+ */
+public class LogManagerTests {
+    static {
+        // Access a logger in initialize the logmanager
+        java.util.logging.Logger.getAnonymousLogger();
+    }
+
+    private final java.util.logging.Level[] levels = {
+            Level.TRACE,
+            Level.DEBUG,
+            Level.INFO,
+            Level.WARN,
+            Level.ERROR,
+            Level.FATAL,
+            java.util.logging.Level.ALL,
+            java.util.logging.Level.FINEST,
+            java.util.logging.Level.FINER,
+            java.util.logging.Level.FINE,
+            java.util.logging.Level.INFO,
+            java.util.logging.Level.CONFIG,
+            java.util.logging.Level.WARNING,
+            java.util.logging.Level.SEVERE,
+            java.util.logging.Level.OFF,
+    };
+
+    @Test
+    public void testLevelReplacement() throws Exception {
+        // Get the static method to find the level by name
+        Method method = null;
+        try {
+            method = java.util.logging.Level.class.getDeclaredMethod("findLevel", String.class);
+            method.setAccessible(true);
+        } catch (NoSuchMethodException ignore) {
+        }
+        // Validate each level
+        for (java.util.logging.Level l : levels) {
+            java.util.logging.Level level = java.util.logging.Level.parse(l.getName());
+            Assert.assertEquals(l, level);
+            if (method != null) {
+                // Hack to check the level by int
+                level = (java.util.logging.Level) method.invoke(null, Integer.toString(level.intValue()));
+                Assert.assertEquals(l, level);
+            }
+        }
+    }
+}
