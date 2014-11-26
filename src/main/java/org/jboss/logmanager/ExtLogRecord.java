a12617a9561cbbe8e61b1123fc52fd247bb7e2a0
Oliver Gondža <ogondza@gmail.com>
2014-11-26 14:27:16 +0100

    Use null to singnal unknown file/class/method name

--- src/main/java/org/jboss/logmanager/ExtLogRecord.java
+++ src/main/java/org/jboss/logmanager/ExtLogRecord.java
@@ -317,10 +317,10 @@ public class ExtLogRecord extends LogRecord {
     }
 
     private void setUnknownCaller() {
-        setSourceClassName("<unknown>");
-        setSourceMethodName("<unknown>");
+        setSourceClassName(null);
+        setSourceMethodName(null);
         setSourceLineNumber(-1);
-        setSourceFileName("<unknown>");
+        setSourceFileName(null);
     }
 
     /**
