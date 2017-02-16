a4ff8911fec29ea7643574bfd7c8e4923757ed60
James Perkins <jperkins@redhat.com>
2017-02-16 13:24:08 -0800

    [LOGMGR-146] Fix the stack trace formatter to properly format suppressed stack traces.

--- src/test/java/org/jboss/logmanager/formatters/FormattersTests.java
+++ src/test/java/org/jboss/logmanager/formatters/FormattersTests.java
@@ -36,7 +36,7 @@ public class FormattersTests {
         String classResourceName = "org/alib/foo/Bar.class";
         URL resource = new URL(null, "jar:file:/D:/Java/SomeDir/alib-3.14.jar!/" + classResourceName, new DummyURLStreamHandler());
 
-        assertEquals(Formatters.getJarName(resource, classResourceName), "alib-3.14.jar");
+        assertEquals(StackTraceFormatter.getJarName(resource, classResourceName), "alib-3.14.jar");
     }
 
     @Test
@@ -44,7 +44,7 @@ public class FormattersTests {
         String classResourceName = "org/blib/foo/Bar.class";
         URL resource = new URL(null, "module:blib-3.14.jar", new DummyURLStreamHandler());
 
-        assertEquals(Formatters.getJarName(resource, classResourceName), "blib-3.14.jar");
+        assertEquals(StackTraceFormatter.getJarName(resource, classResourceName), "blib-3.14.jar");
     }
 
     @Test
@@ -52,7 +52,7 @@ public class FormattersTests {
         String classResourceName = "org/clib/foo/Bar.class";
         URL resource = new URL(null, "vfs:/D:/Java/jboss-as-7.1.1.Final/standalone/deployments/myapp.war/WEB-INF/lib/clib-3.1.4.jar", new DummyURLStreamHandler());
 
-        assertEquals(Formatters.getJarName(resource, classResourceName), "clib-3.1.4.jar");
+        assertEquals(StackTraceFormatter.getJarName(resource, classResourceName), "clib-3.1.4.jar");
     }
 
     @Test
@@ -60,7 +60,7 @@ public class FormattersTests {
         String classResourceName = "org/dlib/foo/Bar.class";
         URL resource = new URL(null, "foo://bar/" + classResourceName, new DummyURLStreamHandler());
 
-        assertEquals(Formatters.getJarName(resource, classResourceName), "");
+        assertEquals(StackTraceFormatter.getJarName(resource, classResourceName), "");
     }
 
     @Test
@@ -68,7 +68,7 @@ public class FormattersTests {
         String classResourceName = "org/elib/foo/Bar.class";
         URL resource = new URL(null, "foo://bar/quux/" + classResourceName, new DummyURLStreamHandler());
 
-        assertEquals(Formatters.getJarName(resource, classResourceName), "quux");
+        assertEquals(StackTraceFormatter.getJarName(resource, classResourceName), "quux");
     }
 
     @Test
@@ -76,7 +76,7 @@ public class FormattersTests {
         String classResourceName = "org/flib/foo/Bar.class";
         URL resource = new URL(null, "foo://bar/baz/quux/" + classResourceName, new DummyURLStreamHandler());
 
-        assertEquals(Formatters.getJarName(resource, classResourceName), "quux");
+        assertEquals(StackTraceFormatter.getJarName(resource, classResourceName), "quux");
     }
 
 }
