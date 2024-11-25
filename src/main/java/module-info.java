894da17b673eb1ff31c2de5502981862d2f94c59
David M. Lloyd <david.lloyd@redhat.com>
2024-11-25 11:06:14 -0600

    Module descriptor updates
    * Add missing exports
    * Add missing service providers
    * Add missing service uses
    * Fix module dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,41 @@
+import org.jboss.logmanager.ConfiguratorFactory;
+import org.jboss.logmanager.JBossLoggerFinder;
+import org.jboss.logmanager.LogContextConfigurator;
+import org.jboss.logmanager.LogContextInitializer;
+import org.jboss.logmanager.LogManager;
+import org.jboss.logmanager.MDCProvider;
+import org.jboss.logmanager.NDCProvider;
+import org.jboss.logmanager.configuration.DefaultConfiguratorFactory;
+
 module org.jboss.logmanager {
-    requires java.logging;
+    requires transitive java.logging;
+
     requires io.smallrye.common.constraint;
     requires io.smallrye.common.cpu;
     requires io.smallrye.common.expression;
     requires io.smallrye.common.net;
     requires io.smallrye.common.os;
     requires io.smallrye.common.ref;
+
     requires static java.xml;
     requires static jakarta.json;
     requires static org.jboss.modules;
+
+    exports org.jboss.logmanager;
+    exports org.jboss.logmanager.configuration;
+    exports org.jboss.logmanager.configuration.filters;
+    exports org.jboss.logmanager.errormanager;
+    exports org.jboss.logmanager.filters;
+    exports org.jboss.logmanager.formatters;
+    exports org.jboss.logmanager.handlers;
+
+    provides java.util.logging.LogManager with LogManager;
+    provides System.LoggerFinder with JBossLoggerFinder;
+    provides ConfiguratorFactory with DefaultConfiguratorFactory;
+
+    uses ConfiguratorFactory;
+    uses LogContextConfigurator;
+    uses LogContextInitializer;
+    uses MDCProvider;
+    uses NDCProvider;
 }
